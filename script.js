55741d47b03a261ee370b87f8c5ba80ce2501330
GAURAV RANA <gaurav.rana43@gmail.com>
2022-09-11 23:12:15 +0530

    Adding id in html,adding .disabled class

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let gbarray = [
   document.getElementById("c1"),
   document.getElementById("c2"),
@@ -10,22 +9,18 @@ let gbarray = [
   document.getElementById("c8"),
   document.getElementById("c9"),
 ];
-function disable(){
-cells.forEach((cell)=>cell.removeEventListener('click',null))
+function disable() {
+  document.getElementById("b").classList.add("disabled");
 }
-function playAgain(){
-  
+function playAgain() {
   for (let i = 0; i < gbarray.length; i++) {
-    gbarray[i].textContent=" ";
+    gbarray[i].textContent = " ";
   }
-  document.getElementById('pc2').textContent="";
-  count=1;
-  document.getElementById('pc1').textContent="Player X turn";
-  
-  
+  document.getElementById("pc2").textContent = "";
+  count = 1;
+  document.getElementById("pc1").textContent = "Player X turn";
+  document.getElementById("b").classList.remove("disabled");
 }
-
-
 function check(cell) {
   console.log(gbarray[1]);
   const cont = document.getElementById("pc2");
@@ -86,16 +81,15 @@ function check(cell) {
     cont.textContent = "O is the Winner";
     disable();
   } else if (
-   (gbarray[0].textContent == "X" ||
-    gbarray[0].textContent == "O") && (gbarray[1].textContent == "X" ||
-    gbarray[1].textContent == "O") && (gbarray[2].textContent == "X" ||
-    gbarray[2].textContent == "O") && (gbarray[3].textContent == "X" ||
-    gbarray[3].textContent == "O") && (gbarray[4].textContent == "X" ||
-    gbarray[4].textContent == "O") && (gbarray[5].textContent == "X" ||
-    gbarray[5].textContent == "O") && (gbarray[6].textContent == "X" ||
-    gbarray[6].textContent == "O") && (gbarray[7].textContent == "X" ||
-    gbarray[7].textContent == "O") && (gbarray[8].textContent == "X" ||
-    gbarray[8].textContent == "O")
+    (gbarray[0].textContent == "X" || gbarray[0].textContent == "O") &&
+    (gbarray[1].textContent == "X" || gbarray[1].textContent == "O") &&
+    (gbarray[2].textContent == "X" || gbarray[2].textContent == "O") &&
+    (gbarray[3].textContent == "X" || gbarray[3].textContent == "O") &&
+    (gbarray[4].textContent == "X" || gbarray[4].textContent == "O") &&
+    (gbarray[5].textContent == "X" || gbarray[5].textContent == "O") &&
+    (gbarray[6].textContent == "X" || gbarray[6].textContent == "O") &&
+    (gbarray[7].textContent == "X" || gbarray[7].textContent == "O") &&
+    (gbarray[8].textContent == "X" || gbarray[8].textContent == "O")
   ) {
     cont.textContent = "It's a tie";
     disable();
@@ -103,7 +97,6 @@ function check(cell) {
     cont.textContent = " ";
   }
 }
-
 
 let count = 1;
 function display(cell) {
@@ -131,9 +124,6 @@ function display(cell) {
 
 const cells = document.querySelectorAll(".cell");
 
-cells.forEach((cell) =>
-  cell.addEventListener("click", ()=>
-  display(cell)
-  ))
-const restart=document.querySelector('#restart');
-restart.addEventListener('click',()=>playAgain());
+cells.forEach((cell) => cell.addEventListener("click", () => display(cell)));
+const restart = document.querySelector("#restart");
+restart.addEventListener("click", () => playAgain());
